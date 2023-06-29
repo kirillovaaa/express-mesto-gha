79028688876a3b15b9cd7a1b5dbd99835cd324e0
Aleksandra Kirillova <kiri2lova@gmail.com>
2023-06-29 10:37:48 +0400

    fix: сделать тест пользователя, схемы и модели

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = 3000;
+
+const usersRouter = require("./routes/users");
+const cardsRouter = require("./routes/cards");
+
+mongoose.connect("mongodb://localhost:27017/mestodb");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: "649d2403aebd5b11db813f0e",
+  };
+
+  next();
+});
+
+app.use(usersRouter);
+app.use(cardsRouter);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
---
+++ routes/users.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+
+const {
+  getAllUsers,
+  getUserById,
+  createUser,
+  // updateUser,
+  // updateAvatar,
+} = require("../controllers/users");
+
+router.get("/users", getAllUsers);
+router.get("/users/:userId", getUserById);
+router.post("/users", createUser);
+// router.patch("users/me", updateUser);
+// router.patch("users/me/avatar", updateAvatar);
+
+module.exports = router;
