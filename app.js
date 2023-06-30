790be5864668a86771f6d48597584af3aa65a0b2
Aleksandra Kirillova <kiri2lova@gmail.com>
2023-06-30 18:36:02 +0400

    fix: перенести юзера в app.js

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
-const { authMiddleware } = require("./middlewares/auth");
-
 const app = express();
 const port = 3000;
 
@@ -15,7 +13,13 @@ mongoose.connect("mongodb://localhost:27017/mestodb");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(authMiddleware);
+// authMiddleware
+app.use((req, res, next) => {
+  req.user = {
+    _id: "649d2403aebd5b11db813f0e",
+  };
+  next();
+});
 
 app.use(usersRouter);
 app.use(cardsRouter);
